6fcbb1ad83424bf329ccd077b7c97d79c7a6ef45
Abdul Lukmana <154068062+abdullukmana@users.noreply.github.com>
2025-06-24 08:42:39 +0700

    Update Auth.php

--- Language/en/Auth.php
+++ Language/en/Auth.php
@@ -1,1 +1,12 @@
+<?php
+declare(strict_types=1);
 
+return [
+    'welcomeBack' => 'Welcome back',
+    'enterDetails' => 'Enter your Untilted account details.',
+    'loginWithGoogle' => 'Log in with Google',
+    'or' => 'or',
+    'useLoginLink' => 'Use a Login Link',
+    'useLoginLinkDesc' => 'Login quickly via a link sent to your email.',
+    'registerWithGoogle' => 'Register with Google',
+];
